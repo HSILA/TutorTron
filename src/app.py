317da8a6b4cca8b54f0ab8fb120f2006c31b86ed
Ali Shiraee <a.shiraee@gmail.com>
2024-10-01 22:16:56 -0400

    fix 3

--- src/app.py
+++ src/app.py
@@ -14,6 +14,10 @@ Last Modified: October 1st, 2024
 import json
 import os
 import sys
+__import__("pysqlite3")
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+import sqlite3
+
 from pathlib import Path
 import streamlit as st
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext
@@ -29,9 +33,6 @@ import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
-__import__("pysqlite3")
-sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
-import sqlite3
 
 def is_unchanged(docs_path, vectordb_path):
     """
